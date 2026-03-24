ef778eb9084abc7d2c442550a8cbb6ae3804145a
tacheraSasi <tacherasasi@gmail.com>
2026-03-24 12:31:08 +0300

    add the constant

--- src/client/constants.ts
+++ src/client/constants.ts
@@ -3,6 +3,7 @@ export const DB_VERSION = 2;
 export const STORE_NAME = "settings";
 export const SETTINGS_KEY = "engines";
 export const THEME_KEY = "theme";
+export const OPEN_IN_NEW_TAB_KEY = "open_in_new_tab";
 export const PER_PAGE = 10;
 export const MAX_PAGE = 10;
 export const BUILTIN_SEARCH_TYPES = new Set([
